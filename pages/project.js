b4fc4ce67f626b9a709977c2fa1254310474a9ce
Rahul jha <jha45507@gmail.com>
2022-11-20 00:42:54 +0530

    remove toast and custom alert and fixed issue

--- pages/project.js
+++ pages/project.js
@@ -20,42 +20,42 @@ export default function Project() {
                                 <img className='border border-[#06b6d4] lg:p-2 sm:w-4/4 lg:w-auto p-5 rounded-2xl' src="/news.png" alt="news" />
                             </a>
                         </div>
-                        <p className=' text-white my-3 text-justify ' > Teck: Strapi Api, ReactJs, NodeJs, JavaScript</p>
-                        <p className=' text-white my-3 text-justify ' > Functionality: Infinite Scroller, Loading bar, Next and Previous button, Design with Card, and click to 'ReadMore' button to open a particular post.</p>
+                        <p className=' text-white my-3 text-justify'> <span className='font-bold'>Tech</span> Strapi Api, ReactJs, NodeJs, JavaScript</p>
+                        <p className='text-white my-3 text-justify'> <span className='font-bold'>Functionality</span> Infinite Scroller, Loading bar, Next and Previous button, Design with Card, and click to ReadMore button to open a particular post.</p>
                     </div>
 
-                    <div className='mx-auto lg:mx-10 border sm:w-3/4 border-pink-300 px-5 py-5 rounded-lg projectbox mb-20 '>
+                    <div className='mx-auto lg:mx-10 border sm:w-3/4 border-pink-300 px-5 py-5 rounded-lg projectbox mb-20'>
 
                         <div className=''>
                             <a target="_blank" rel="noopener noreferrer" href={'https://with-react-text-editor.netlify.app'} >
                                 <img className='border border-[#06b6d4] lg:p-2 sm:w-4/4 lg:w-auto p-5  rounded-2xl' src="/editor.png" alt="editer" />
                             </a>
                         </div>
-                        <p className=' text-white my-3 text-justify ' > Teck: ReactJs, JavaScript, NodeJs, </p>
-                        <p className=' text-white my-3 text-justify ' > Functionality: with text can be uppercase, lowercase, reverse, copy, clear extra spaces, capital first letter, count words, count characters, and show preview.</p>
+                        <p className='text-white my-3 text-justify'> <span className='font-bold'>Tech</span> ReactJs, JavaScript, NodeJs, </p>
+                        <p className='text-white my-3 text-justify'> <span className='font-bold'>Functionality</span> with text can be uppercase, lowercase, reverse, copy, clear extra spaces, capital first letter, count words, count characters, and show preview.</p>
                     </div>
 
-                    <div className='mx-auto lg:mx-10 border sm:w-3/4 border-pink-300 px-5 py-5 rounded-lg projectbox mb-20 '>
+                    <div className='mx-auto lg:mx-10 border sm:w-3/4 border-pink-300 px-5 py-5 rounded-lg projectbox mb-20'>
                         <div className=''>
                             <a target="_blank" rel="noopener noreferrer" href={'https://soft-notes-book-with-mern.netlify.app'} >
                                 <img className='border border-[#06b6d4] lg:p-2 sm:w-4/4 lg:w-auto p-5  rounded-2xl' src="/notes.png" alt="notes" />
                             </a>
                         </div>
-                        <p className=' text-white my-3 text-justify ' > Teck: MongoDB, ExpressJs, ReactJs, NodeJs (MERN) </p>
-                        <p className=' text-white my-3 text-justify ' > Functionality: create notes, update notes, read notes, delete notes, and with an authentication token save notes.</p>
+                        <p className='text-white my-3 text-justify'> <span className='font-bold'>Tech</span> MongoDB, ExpressJs, ReactJs, NodeJs (MERN) </p>
+                        <p className='text-white my-3 text-justify'> <span className='font-bold'>Functionality</span> create notes, update notes, read notes, delete notes, and with an authentication token save notes.</p>
                     </div>
 
                 </div>
 
-                <div className=' lg:flex mx-5 lg:my-10 lg:w-[60%] w-full'>
-                    <div className='mx-auto lg:mx-10 border sm:w-3/4 border-pink-300 px-5 py-5 rounded-lg projectbox mb-20 '>
+                <div className='lg:flex mx-5 lg:my-10 lg:w-[60%] w-full'>
+                    <div className='mx-auto lg:mx-10 border sm:w-3/4 border-pink-300 px-5 py-5 rounded-lg projectbox mb-20'>
                         <div className=''>
                             <a target="_blank" rel="noopener noreferrer" href={'https://events-calendar-javascript-project.netlify.app'} >
                                 <img className='border border-[#06b6d4] lg:p-2 sm:w-4/4 lg:w-auto p-5  rounded-2xl' src="/calendar.png" alt="calendar" />
                             </a>
                         </div>
-                        <p className=' text-white my-3 text-justify ' > Teck: JavaScript, Bootstrap</p>
-                        <p className=' text-white my-3 text-justify ' > Functionality: In this events calendar Add Entries, Read Entries, Delete Entries, and Save Entries with Start date, End date, and Time. They save to local storage.</p>
+                        <p className='text-white my-3 text-justify'> <span className='font-bold'>Tech</span> JavaScript, Bootstrap</p>
+                        <p className='text-white my-3 text-justify'> <span className='font-bold'>Functionality</span> In this events calendar Add Entries, Read Entries, Delete Entries, and Save Entries with Start date, End date, and Time. They save to local storage.</p>
                     </div>
 
                     <div className='mx-auto lg:mx-10 border sm:w-3/4 border-pink-300 px-5 py-5 rounded-lg projectbox mb-20 '>
@@ -64,8 +64,8 @@ export default function Project() {
                                 <img className='border border-[#06b6d4] lg:p-2 sm:w-4/4 lg:w-auto p-5  rounded-2xl' src="/clock.png" alt="clock" />
                             </a>
                         </div>
-                        <p className=' text-white my-3 text-justify ' > Teck: Html, Css, JavaScript </p>
-                        <p className=' text-white my-3 text-justify ' > Functionality: both functions are the Digital clock and can wall clock and set the alarm.</p>
+                        <p className='text-white my-3 text-justify'> <span className='font-bold'>Tech</span> Html, Css, JavaScript </p>
+                        <p className='text-white my-3 text-justify'> <span className='font-bold'>Functionality</span> both functions are the Digital clock and can wall clock and set the alarm.</p>
                     </div>
                 </div>
 
